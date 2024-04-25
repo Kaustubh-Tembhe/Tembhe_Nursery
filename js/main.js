1bcff01ef69293655b00d966afdae7d6b9bb61e1
Kaustubh Tembhe <121222001+Kaustubh-Tembhe@users.noreply.github.com>
2024-04-25 14:43:42 +0530

    update Existing code

--- js/main.js
+++ js/main.js
@@ -129,14 +129,14 @@ themeButton.addEventListener('click', () => {
 const sr = ScrollReveal({
     origin: 'top',
     distance: '60px',
-    duration: 2500,
-    delay: 400,
+    duration: 2200,
+    delay: 200,
     // reset: true
 })
 
 sr.reveal(`.home__data`)
-sr.reveal(`.home__img`, {delay: 500})
-sr.reveal(`.home__social`, {delay: 600})
+sr.reveal(`.home__img`, {delay: 200})
+sr.reveal(`.home__social`, {delay: 200})
 sr.reveal(`.about__img, .contact__box`,{origin: 'left'})
 sr.reveal(`.about__data, .contact__form`,{origin: 'right'})
-sr.reveal(`.steps__card, .product__card, .questions__group, .footer`,{interval: 100})
+sr.reveal(`.steps__card, .product__card, .questions__group, .footer`,{interval: 50})
